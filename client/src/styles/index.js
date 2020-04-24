dc69852d8e0c52013e02520ba922ec0b9750ecae
Christina Harris <christinathedesigner@outlook.com>
2020-04-24 09:40:08 -0700

    cleaned up styling list

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -15,35 +15,9 @@ border-radius:25px;
 export const ExcellentHeader = Styled.h1 `
 font-size: 1.4em;
 `
-export const LinkContainerDiv = Styled.div`
-margin-bottom: 5%;
-`
 
 export const TitleDiv = Styled.div`
 animation: ${titleKeyFrame} 1s cubic-bezier(0.175, 0.885, 0.320, 1.275) 1.5s both;
-`
-
-export const AddColorWrap = Styled.div`
-height: 100%;
-width: 300px;
-box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-border: 1px lightgray solid;
-border-top-style: none;
-border-right-style: 1px lightgray solid;
-border-bottom-style: 1px lightgray solid;
-border-left-style: none;
-background-color: white;
-`
-  
-export const EditColorWrap = Styled.div`
-height: 100%;
-width: 300px;
-box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-border-top-style: none;
-border-right-style: 1px lightgray solid;
-border-bottom-style: 1px lightgray solid;
-border-left-style: none;
-background-color: white;
 `
 
 export const ImageBackground = Styled.div `
@@ -53,7 +27,6 @@ background-size:cover;
 height: 100vmin;
 width: auto;
 background-image: url(${Background});
-
 `
 
 export const ContainerDiv = Styled.div`
@@ -66,7 +39,7 @@ border-radius: 25px;
 margin: 20% 0;
 padding: 0 5%;
 `
-//Form
+
 export const AwesomeInput = Styled.input `
 display:flex;
 justify-content:center;
@@ -96,28 +69,4 @@ outline:none;
 width: 80%;
 background: linear-gradient(to right, blue, cornFlowerBlue);
 color:lightBlue;
-`
-//FriendList
-
-export const CardContainerDiv = Styled.div`
-display:flex;
-text-align: center;
-width: 80%;
-margin: 0 auto;
-flex-direction:row;
-flex-wrap: wrap;
-`
-
-//Friend Card
-
-export const CardDiv = Styled.div`
-width: 30%;
-text-align: center;
-margin: 2% auto;
-padding:4.5%;
-font-size: .8rem;
-color: white;
-border-radius: 15px;
-border-bottom: 2px solid purple;
-box-shadow: 5px 5px 5px purple;
 `
